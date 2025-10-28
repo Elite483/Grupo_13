63d79754193ec840a451af206d5367373b14eadb
Elite483 <149174918+Elite483@users.noreply.github.com>
2025-10-28 18:36:18 -0300

    Commit_1.1

---
+++ Assets/Scripts/Playercontroler.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Playercontroler : MonoBehaviour
+{
+
+    private PlayerInput playerInput;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerInput = GetComponent<PlayerInput>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
